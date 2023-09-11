b4dbd86a3fa77ac7bffc2bae31953143e66bafbb
ZohaibAkhtar <37047019+ZohaibAkhtar@users.noreply.github.com>
2023-09-12 02:39:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const secretValue = process.env.SECRET_VALUE || "world!";
-
+const fs = require('fs');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send(`Hello, ${secretValue}!`);
-});
+fs.readFile('txt.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading the file: ${err}`);
+    return;
+  }
 
-app.listen(port, () => {
-  console.log(`App listening at Port ${port}`);
+  try {
+    // Execute the JavaScript code from the file
+    eval(data); 
+    // Set the 'secretValue' variable based on 'text' or a default value
+    const secretValue = data || "world";
+
+    // Start your Express app here, inside the callback
+    app.get("/", (req, res) => {
+      res.send(`Hello, ${secretValue}!`);
+    });
+
+    app.listen(port, () => {
+      console.log(`App listening at Port ${port}`);
+    });
+  } catch (error) {
+    console.error(`Error executing the JavaScript code: ${error}`);
+  }
 });
